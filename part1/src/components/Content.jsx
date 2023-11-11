f1d7fffa90e433cf957e8bf919a01bdb59cd58f0
LARBI Ishak <ishak.larbi@inttic.dz>
2023-11-11 15:17:37 +0100

    Exercice 1.2

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,15 +1,17 @@
 export default function Content({ parts, exercises }) {
   return (
     <>
-      <p>
-        {parts[0]} {exercises[0]}
-      </p>
-      <p>
-        {parts[1]} {exercises[1]}
-      </p>
-      <p>
-        {parts[2]} {exercises[2]}
-      </p>
+      <Part part={parts[0]} exercise={exercises[0]} />
+      <Part part={parts[1]} exercise={exercises[1]} />
+      <Part part={parts[2]} exercise={exercises[2]} />
     </>
+  );
+}
+
+function Part({ part, exercise }) {
+  return (
+    <p>
+      {part} {exercise}
+    </p>
   );
 }
