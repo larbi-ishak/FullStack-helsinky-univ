5f8545c57c577104a8ffb26b65e331b614f03518
LARBI Ishak <ishak.larbi@inttic.dz>
2023-11-12 22:00:07 +0100

    Exercise 2.2

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,18 +1,23 @@
 export default function Course({ course }) {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       <h1>{course.name}</h1>
       <Parts parts={course.parts} />
+      <Total total={total} />
     </div>
-  )
+  );
 }
 
 function Parts({ parts }) {
   return (
     <>
-      {parts.map(part => <Part key={part.id} part={part} />)}
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </>
-  )
+  );
 }
 
 function Part({ part }) {
@@ -20,6 +25,13 @@ function Part({ part }) {
     <p>
       {part.name} {part.exercises}
     </p>
-  )
+  );
 }
 
+function Total({ total }) {
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+}
