339a5e5f3850510aa40c6bd58268080e74fa670c
LARBI Ishak <ishak.larbi@inttic.dz>
2023-11-19 11:38:15 +0100

    Exercise 2.9

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-12345567" },
+  const [store, setStore] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [persons, setPersons] = useState(store);
   const [person, setPerson] = useState({ name: "", number: "" });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    console.log(event.target);
     setPerson({ ...person, [name]: value });
   };
 
@@ -26,12 +29,27 @@ const App = () => {
       number: person.number,
     };
     setPersons(persons.concat(personObject));
+    // update the store
+    setStore(store.concat(personObject));
     setPerson({ name: "", number: "" });
+  };
+
+  const handleFilter = (event) => {
+    const { value } = event.target;
+    const Optvalue = value.trim().toLowerCase();
+    const filtered = store.filter((person) =>
+      person.name.toLowerCase().includes(Optvalue),
+    );
+    setPersons(filtered);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter show with: <input type="text" onChange={handleFilter} />{" "}
+      </div>
       <form onSubmit={handleAddPerson}>
         <div>
           name:{" "}
